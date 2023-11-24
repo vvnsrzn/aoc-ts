086215c315368d850a2e2dbe49c5a3cfd179d72c
Vivian Sarazin <vivian.sarazin@zenika.com>
2023-11-24 09:06:29 +0100

    solved!

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { fetchAndWriteChallenge, readPuzzle } from "./libs";
+import { computeSum, fetchAndWriteChallenge, readPuzzle } from "./libs";
 
 /**
  * Fonction principale
@@ -15,9 +15,26 @@ async function main() {
  * Fonction pour résoudre le puzzle
  * Testable dans index.spec.ts
  */
-export function solver(data: string[]) {
+export function solver(data: string[]): number {
   // écris ton code ici !
-  return data;
+  const res: number[] = [];
+  let temp: number[] = [];
+  for (const el of data) {
+    if (el === "") {
+      res.push(computeSum(temp));
+      temp = [];
+    } else {
+      temp.push(Number(el));
+    }
+  }
+  // Réponse A
+  // return Math.max(...res);
+
+  // Réponse B
+  return new Int32Array(res)
+    .sort()
+    .slice(res.length - 3, res.length)
+    .reduce((prev, curr) => prev + curr);
 }
 
 main();
