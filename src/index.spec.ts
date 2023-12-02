c9ed9e4344b40bd9ba04b53d747954e796c2b7af
Vivian Sarazin <vivian.sarazin@zenika.com>
2023-12-02 06:43:18 +0100

    solved!

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,9 +4,9 @@ import { postAnswer, readPuzzle } from "../src/libs";
 import { describe, expect, test } from "vitest";
 
 describe("AoC", () => {
-  test("Spec #1", async () => {
-    const input = readPuzzle(specFile(1)); // vérifier l'id de la spec à tester ! (inputs/year)
-    expect(solver(input)).toEqual(142);
+  test.only("Spec #1", async () => {
+    const input = readPuzzle(specFile(17)); // vérifier l'id de la spec à tester ! (inputs/year)
+    expect(solver(input)).toEqual(281);
 
     if (hxc) {
       const data = readPuzzle();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { computeSum, fetchAndWriteChallenge, readPuzzle } from "./libs";
+import consola from "consola";
+import {
+  computeMatrixSum,
+  computeSum,
+  fetchAndWriteChallenge,
+  readPuzzle,
+} from "./libs";
 
 /**
  * Fonction principale
@@ -11,26 +17,93 @@ async function main() {
   solver(data);
 }
 
+const letters = [
+  {
+    digits: ["one", "1"],
+    number: 1,
+  },
+  {
+    digits: ["two", "2"],
+    number: 2,
+  },
+  {
+    digits: ["three", "3"],
+    number: 3,
+  },
+  {
+    digits: ["four", "4"],
+    number: 4,
+  },
+  {
+    digits: ["five", "5"],
+    number: 5,
+  },
+  {
+    digits: ["six", "6"],
+    number: 6,
+  },
+  {
+    digits: ["seven", "7"],
+    number: 7,
+  },
+  {
+    digits: ["eight", "8"],
+    number: 8,
+  },
+  {
+    digits: ["nine", "9"],
+    number: 9,
+  },
+];
+
 /**
  * Fonction pour résoudre le puzzle
  * Testable dans index.spec.ts
  */
 export function solver(data: string[]): number {
   const result: number[] = [];
-  for (const dataElement of data) {
-    const element = dataElement
-      .split("")
-      .filter((e) => {
-        return parseFloat(e);
-      })
-      .map(Number);
+  for (const str of data) {
+    const res = [];
+    const leftCandidate = [];
+    const rightCandidate = [];
+    const array = str.split("");
+    for (let i = 0; i < array.length; i++) {
+      const element = array[i];
+      leftCandidate.push(element);
+      const substring = leftCandidate.join("");
+      const left = newFunction(substring)!;
+      if (left) {
+        res.push(left);
+        break;
+      }
+    }
 
-    const temp = Number([element[0], element[element.length - 1]].join(""));
+    for (let j = array.length - 1; j >= 0; j--) {
+      const element = array[j];
+      rightCandidate.push(element);
+      const substring = rightCandidate.join("").split("").reverse().join("");
+      const right = newFunction(substring)!;
+      if (right) {
+        res.push(right);
+        break;
+      }
+    }
 
-    result.push(temp);
+    result.push(Number(res.join("")));
   }
-  console.log(computeSum(result));
-  return computeSum(result);
+
+  console.log(result);
+  const tutu = computeSum(result);
+  return tutu;
 }
 
-main();
+function newFunction(substring: string) {
+  for (const { digits, number } of letters) {
+    for (const digit of digits) {
+      if (substring.includes(digit)) {
+        return number;
+      }
+    }
+  }
+}
+// main();
