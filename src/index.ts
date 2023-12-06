2bbf5e92f557ac9a366ca5499f540291b6e0f9e3
Vivian Sarazin <vivian.sarazin@zenika.com>
2023-12-07 00:45:24 +0100

    solved!

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
-import { fetchAndWriteChallenge, readPuzzle } from "./libs";
-
-/**
- * Fonction principale
- * Vérifie si le puzzle est bien chargé
- * Dans le cas contraire, téléchargement et écriture du puzzle
- */
-async function main() {
-  await fetchAndWriteChallenge();
-  const data = readPuzzle();
-  solver(data);
-}
-
 /**
  * Fonction pour résoudre le puzzle
  * Testable dans index.spec.ts
  */
-export function solver(data: string[]) {
-  for (const line of data) {
-    console.log(line);
+export function solver() {
+  const input = [
+    {
+      time: 42_899_189,
+      distance: 308_117_012_911_467,
+    },
+  ];
+  const res = [];
+  for (const race of input) {
+    let temp = 0;
+    for (let h = 0; h <= race.time; h++) {
+      const x = race.time - h;
+      x * h > race.distance && temp++;
+    }
+    res.push(temp);
   }
-  return 1;
+  console.log(res.reduce((prev, curr) => prev * curr));
 }
-
-main();
