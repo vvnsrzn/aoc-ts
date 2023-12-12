f38fd6df3231b6ac4aceb06759d76f61ded19b37
Vivian Sarazin <vivian.sarazin@zenika.com>
2023-12-12 23:45:29 +0100

    part I solved!

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,17 +1,36 @@
 import { hxc, specFile } from "./constants";
-import { solver } from "./index";
+import { solver, getPossibleArangements } from "./index";
 import { postAnswer, readPuzzle } from "./libs";
 import { describe, expect, test } from "vitest";
 
-describe("AoC", () => {
+describe("Solver", () => {
+  test("???.### -> 1,1,3 equal 1", () => {
+    const given =
+      "???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3";
+    expect(getPossibleArangements(given)).toEqual(1);
+  });
+  test(".??..??...?##. -> 1,1,3 equal 4", () => {
+    const given = ".??..??...?##. 1,1,3";
+    expect(getPossibleArangements(given)).toEqual(4);
+  });
+  test("?#?#?#?#?#?#?#? -> 1,3,1,6 equal 1", () => {
+    const given = "?#?#?#?#?#?#?#? 1,3,1,6";
+    expect(getPossibleArangements(given)).toEqual(1);
+  });
+  test("????.#...#... -> 4,1,1 equal 1", () => {
+    const given = "????.#...#... 4,1,1";
+    expect(getPossibleArangements(given)).toEqual(1);
+  });
+  test("????.######..#####. -> 1,6,5 equal 4", () => {
+    const given = "????.######..#####. 1,6,5";
+    expect(getPossibleArangements(given)).toEqual(4);
+  });
+  test("?###???????? -> 3,2,1 equal 10", () => {
+    const given = "?###???????? 3,2,1";
+    expect(getPossibleArangements(given)).toEqual(10);
+  });
   test("Spec #1", async () => {
-    const input = readPuzzle(specFile(3)); // vérifier l'id de la spec à tester ! (inputs/year)
-    expect(solver(input)).toEqual(-Infinity);
-
-    if (hxc) {
-      const data = readPuzzle();
-      const candidate = solver(data);
-      await postAnswer(candidate);
-    }
+    const input = readPuzzle(specFile(1)); // vérifier l'id de la spec à tester ! (inputs/year)
+    expect(solver(input)).toEqual(21);
   }, 333_333_333); // mes chiffres porte-bonheur, à la discrétion du développeur, mais c'est aussi un timeout :)
 });
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,105 @@
-import { fetchAndWriteChallenge, readPuzzle } from "./libs";
+// import { fetchAndWriteChallenge, readPuzzle } from "./libs";
 
 /**
  * Fonction principale
  * Vérifie si le puzzle est bien chargé
  * Dans le cas contraire, téléchargement et écriture du puzzle
  */
-async function main() {
-  await fetchAndWriteChallenge();
-  const data = readPuzzle();
-  solver(data);
-}
+// async function main() {
+//   await fetchAndWriteChallenge();
+//   const data = readPuzzle();
+//   solver(data);
+// }
+// main();
 
 /**
  * Fonction pour résoudre le puzzle
  * Testable dans index.spec.ts
  */
 export function solver(data: string[]) {
+  let res = 0;
   for (const line of data) {
-    console.log(line);
+    res += getPossibleArangements(line);
   }
-  return 1;
+  return res;
 }
 
-main();
+export function getPossibleArangements(line: string): number {
+  const [left, right] = line.split(" ");
+  const pattern = right.split(",").map(Number);
+
+  const seq: Character[] = left
+    .split("")
+    .filter((el): el is Character => characters.includes(el));
+
+  const result = dfs(seq, pattern);
+
+  if (result) return result?.size;
+  return 0;
+}
+
+function dfs(
+  seq: Character[],
+  pattern: number[],
+  start: number = 0,
+  graph: Map<string, Character[]> = new Map(),
+  winners: Set<string> = new Set()
+) {
+  const next = seq[start];
+  if (!next) return;
+  const possibleNextCharacters = getPossibleNextCharacters(next);
+  const candidates = [];
+
+  for (const possibleNextCharacter of possibleNextCharacters) {
+    const candidate = [
+      ...seq.slice(0, start),
+      possibleNextCharacter,
+      ...seq.slice(start + 1),
+    ];
+    candidates.push(candidate);
+  }
+
+  for (const candidate of candidates) {
+    const key = candidate.join("");
+
+    if (isMatchingPattern(candidate, pattern)) {
+      winners.add(candidate.join(""));
+    }
+
+    if (!graph.has(key)) {
+      graph.set(key, seq);
+    }
+
+    dfs(candidate, pattern, start + 1, graph, winners);
+  }
+
+  return winners;
+}
+
+type Character = "." | "#" | "?";
+const characters = [".", "#", "?"];
+
+function getPossibleNextCharacters(character: Character): Character[] {
+  if (character === "?") {
+    return ["#", "."];
+  } else {
+    return [character];
+  }
+}
+
+function isMatchingPattern(seq: Character[], pattern: number[]): boolean {
+  const challenge = seq
+    .join("")
+    .split(".")
+    .filter((v) => v);
+  if (challenge.length === pattern.length) {
+    for (let i = 0; i < pattern.length; i++) {
+      const p = pattern[i];
+      if (challenge[i] !== "#".repeat(p)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
