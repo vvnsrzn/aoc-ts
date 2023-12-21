c6e9eb6726a851a8403dd46e18d73fe5b551151d
Vivian Sarazin <vivian.sarazin@zenika.com>
2023-12-21 17:20:45 +0100

    yolo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { writeFile, writeFileSync } from "fs";
 import { fetchAndWriteChallenge, readPuzzle } from "./libs";
 
 /**
@@ -10,16 +11,97 @@ async function main() {
   const data = readPuzzle();
   solver(data);
 }
+// main();
 
 /**
  * Fonction pour résoudre le puzzle
  * Testable dans index.spec.ts
  */
 export function solver(data: string[]) {
-  for (const line of data) {
-    console.log(line);
+  const map = new Map<number, Set<number>>();
+  const lastPosition = { x: 0, y: 0 };
+  for (const [, line] of data.entries()) {
+    const { direction, meters } = parseInstructions(line);
+    if (direction === "R") {
+      const limit = lastPosition.x + meters;
+      const start = lastPosition.x;
+      const values = [];
+      for (let i = start; i <= limit; i++) {
+        values.push(i);
+      }
+      if (map.has(lastPosition.y)) {
+        const currentValues = map.get(lastPosition.y);
+        if (currentValues)
+          map.set(lastPosition.y, new Set([...currentValues, ...values]));
+      } else {
+        map.set(lastPosition.y, new Set([...values]));
+      }
+      lastPosition.x = Math.max(...values);
+    }
+    if (direction === "L") {
+      const limit = lastPosition.x - meters;
+      const start = lastPosition.x;
+      const values = [];
+      for (let i = start; i >= limit; i--) {
+        values.push(i);
+      }
+      if (map.has(lastPosition.y)) {
+        const currentValues = map.get(lastPosition.y);
+        if (currentValues)
+          map.set(lastPosition.y, new Set([...currentValues, ...values]));
+      } else {
+        map.set(lastPosition.y, new Set([...values]));
+      }
+      lastPosition.x = Math.min(...values);
+    }
+    if (direction === "U") {
+      const limit = lastPosition.y - meters;
+      const start = lastPosition.y;
+      for (let i = start; i >= limit; i--) {
+        lastPosition.y = i;
+        if (map.has(lastPosition.y)) {
+          const currentValues = map.get(lastPosition.y);
+          if (currentValues)
+            map.set(
+              lastPosition.y,
+              new Set([...currentValues, lastPosition.x])
+            );
+        } else {
+          map.set(lastPosition.y, new Set([lastPosition.x]));
+        }
+      }
+    }
+    if (direction === "D") {
+      const limit = meters + lastPosition.y;
+      const start = lastPosition.y;
+      for (let i = start; i <= limit; i++) {
+        lastPosition.y = i;
+        if (map.has(lastPosition.y)) {
+          const currentValues = map.get(lastPosition.y);
+          if (currentValues)
+            map.set(
+              lastPosition.y,
+              new Set([...currentValues, lastPosition.x])
+            );
+        } else {
+          map.set(lastPosition.y, new Set([lastPosition.x]));
+        }
+      }
+    }
   }
-  return 1;
+  let total = 0;
+  map.forEach((value, key) => {
+    console.log(key, value);
+    total += Math.max(...value) - Math.min(...value) + 1;
+  });
+  return total;
 }
 
-main();
+type Direction = "U" | "D" | "L" | "R";
+function parseInstructions(line: string): {
+  direction: Direction;
+  meters: number;
+} {
+  const [direction, meters] = line.split(" ") as [Direction, string];
+  return { direction, meters: Number(meters) };
+}
