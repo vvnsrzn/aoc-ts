4ac50d3e3e6b38d2d7a568445a3f186e33e1010d
Vivian Sarazin <vivian.sarazin@zenika.com>
2023-12-22 14:50:47 +0100

    part I solved!

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,17 +1,38 @@
 import { hxc, specFile } from "./constants";
-import { solver } from "./index";
+import { solver, sorter } from "./index";
 import { postAnswer, readPuzzle } from "./libs";
 import { describe, expect, test } from "vitest";
 
+describe("Row", () => {
+  test.each([
+    {
+      input: ["O", "O", ".", "O", ".", "O", ".", ".", "#", "#"],
+      expected: ["O", "O", "O", "O", ".", ".", ".", ".", "#", "#"],
+    },
+    {
+      input: [".", ".", ".", "O", "O", ".", ".", ".", ".", "O"],
+      expected: ["O", "O", "O", ".", ".", ".", ".", ".", ".", "."],
+    },
+    {
+      input: [".", "#", ".", "O", ".", "#", "O", ".", ".", "."],
+      expected: [".", "#", "O", ".", ".", "#", "O", ".", ".", "."],
+    },
+  ])("sorter(%p) returns %s", ({ input, expected }) => {
+    const given = sorter(input);
+    expect(given).toStrictEqual(expected);
+  });
+});
+
 describe("AoC", () => {
   test("Spec #1", async () => {
-    const input = readPuzzle(specFile(3)); // vérifier l'id de la spec à tester ! (inputs/year)
-    expect(solver(input)).toEqual(-Infinity);
+    const input = readPuzzle(specFile(4)); // vérifier l'id de la spec à tester ! (inputs/year)
+    expect(solver(input)).toEqual(136);
 
     if (hxc) {
       const data = readPuzzle();
       const candidate = solver(data);
-      await postAnswer(candidate);
+      expect(candidate).toBe(110274);
+      // await postAnswer(candidate);
     }
   }, 333_333_333); // mes chiffres porte-bonheur, à la discrétion du développeur, mais c'est aussi un timeout :)
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { fetchAndWriteChallenge, readPuzzle } from "./libs";
+import { fetchAndWriteChallenge, readPuzzle, selectMatrixColumn } from "./libs";
 
 /**
  * Fonction principale
@@ -11,15 +11,42 @@ async function main() {
   solver(data);
 }
 
+// main();
+
 /**
  * Fonction pour résoudre le puzzle
  * Testable dans index.spec.ts
  */
 export function solver(data: string[]) {
-  for (const line of data) {
-    console.log(line);
+  let result = 0;
+  for (let index = 0; index < data.length; index++) {
+    const col = selectMatrixColumn(data, index);
+    const sorted = sorter(col);
+    const res = counter(sorted);
+    result += res;
   }
-  return 1;
+  return result;
 }
 
-main();
+export function sorter(data: string[]): string[] {
+  return data
+    .join("")
+    .split("#")
+    .flatMap((item) => [item, "#"])
+    .slice(0, -1)
+    .filter((el) => el)
+    .flatMap((el) => {
+      if (el.length === 0) return ["#"];
+      return el.split("").sort().reverse();
+    });
+}
+
+function counter(sorted: string[]): number {
+  const length = sorted.length;
+  return sorted.reduce((acc, curr, index) => {
+    if (curr === "O") {
+      acc += length - index;
+    }
+    return acc;
+  }, 0);
+}
