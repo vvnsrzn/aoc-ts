c2fb94f4d875fe35b0e0d0a0574acb5b53414d41
Vivian <vivian.sarazin@hublo.com>
2024-12-01 06:37:27 +0100

    solved!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { fetchAndWriteChallenge, readPuzzle } from "./libs/puzzle/index.ts";
-
+import { selectMatrixColumn } from "./libs/matrix/index.ts";
 /**
  * Fonction principale
  * Vérifie si le puzzle est bien chargé
@@ -16,10 +16,17 @@ async function main() {
  * Testable dans index.spec.ts
  */
 export function solver(data: string[]) {
+  const matrix = []
   for (const line of data) {
-    console.log(line);
+    matrix.push(line.split("   "));
   }
-  return 1;
+  const leftList = selectMatrixColumn(matrix, 0).map(Number).toSorted((a, b) => a - b)
+  const rightList = selectMatrixColumn(matrix, 1).map(Number).toSorted((a, b) => a - b)
+  const res = [];
+  for (const leftItem of leftList) {
+    res.push([leftItem, rightList.filter(el => el === leftItem).length]);
+  }
+  return res.reduce((prev, curr) => prev + curr[0] * curr[1], 0);
 }
 
 main();
