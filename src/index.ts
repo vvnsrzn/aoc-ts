997095692958bffe17e6ea6644b8cc7cbe73befb
Vivian <vivian.sarazin@hublo.com>
2024-12-06 08:52:33 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { fetchAndWriteChallenge, readPuzzle } from "./libs/puzzle/index.ts";
-
+import { Directions, selectMatrixColumn, selectMatrixRow } from "./libs/matrix/index.ts";
 /**
  * Fonction principale
  * Vérifie si le puzzle est bien chargé
@@ -15,11 +15,51 @@ async function main() {
  * Fonction pour résoudre le puzzle
  * Testable dans index.spec.ts
  */
+type Tile = "." | "#" | Guard
+type Guard = ">" | "<" | "^" | "v"
+
+function guardDirection(guard: Guard): Directions {
+  switch (guard) {
+    case "<":
+      return "west"
+    case ">":
+      return "east"
+    case "^":
+      return "north"
+    case "v":
+      return "south"
+  }
+}
+
+function isGuard(pos: Tile): boolean {
+  return [">", "<", "^", "v"].includes(pos)
+}
+
 export function solver(data: string[]) {
+  const matrix: Tile[][] = []
   for (const line of data) {
-    console.log(line);
+    const splittedLines = line.split('') as Tile[]
+    matrix.push(splittedLines)
+  }
+
+  for (let y = 0; y < matrix.length; y++) {
+    const row = matrix[y];
+    for (let x = 0; x < row.length; x++) {
+      const pos = row[x];
+      if (isGuard(pos)) {
+        if (pos === "<" || pos === ">") {
+          // const currentRow = selectMatrixRow(matrix, y)
+        }
+        if (pos === "^" || pos === "v") {
+          const currentCol = selectMatrixColumn(matrix, x)
+          const obstacle = currentCol.indexOf("#")
+          console.log(pos, { x, y }, obstacle)
+        }
+      }
+    }
+
   }
   return 1;
 }
 
-main();
+// main();
