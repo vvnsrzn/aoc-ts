38e9984208a0f5833cfd87e81ebc9421294d98df
Vivian SARAZIN <9358572+vvnsrzn@users.noreply.github.com>
2024-12-06 13:56:18 +0100

    wip

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,13 +5,16 @@ import { postAnswer, readPuzzle } from "./libs/puzzle/index.ts";
 
 describe("AoC", () => {
   test("Spec #1", async () => {
-    const input = readPuzzle(specFile(1)); // vérifier l'id de la spec à tester ! (inputs/year)
-    expect(solver(input)).toEqual(41);
+    // const input = readPuzzle(specFile(1)); // vérifier l'id de la spec à tester ! (inputs/year)
+    // expect(solver(input)).toEqual(41);
 
     if (hxc) {
       const data = readPuzzle();
       const candidate = solver(data);
-      await postAnswer(candidate);
+      console.log({ candidate })
+      if (candidate && candidate > 142) {
+        // await postAnswer(candidate);
+      }
     }
   }, 333_333_333); // mes chiffres porte-bonheur, à la discrétion du développeur, mais c'est aussi un timeout :)
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { fetchAndWriteChallenge, readPuzzle } from "./libs/puzzle/index.ts";
-import { Directions, selectMatrixColumn, selectMatrixRow } from "./libs/matrix/index.ts";
+import { type Directions, selectMatrixColumn, selectMatrixRow } from "./libs/matrix/index.ts";
+import { absDifference } from "./libs/math/index.ts";
+import { writeFile } from "fs";
 /**
  * Fonction principale
  * Vérifie si le puzzle est bien chargé
@@ -18,24 +20,48 @@ async function main() {
 type Tile = "." | "#" | Guard
 type Guard = ">" | "<" | "^" | "v"
 
-function guardDirection(guard: Guard): Directions {
-  switch (guard) {
-    case "<":
-      return "west"
-    case ">":
-      return "east"
-    case "^":
-      return "north"
-    case "v":
-      return "south"
-  }
-}
-
 function isGuard(pos: Tile): boolean {
   return [">", "<", "^", "v"].includes(pos)
 }
 
+function nextGuard(guard: Guard): Guard {
+  switch (guard) {
+    case "^":
+      return ">"
+    case "<":
+      return "^"
+    case ">":
+      return "v"
+    case "v":
+      return "<"
+  }
+}
+
+type Position = { x: number, y: number }
+
+
+
 export function solver(data: string[]) {
+  function addTiles(start: Position, end: Position): void {
+    const { y: yStart, x: xStart } = start
+    const { y: yEnd, x: xEnd } = end
+    if (xStart === xEnd) {
+      let y = absDifference(yStart, yEnd)
+      do {
+        y--
+        res.add(`${xStart}-${y}`)
+      } while (y > 1);
+    }
+    if (yStart === yEnd) {
+      let x = absDifference(xStart, xEnd)
+      do {
+        x--
+        res.add(`${x}-${yStart}`)
+      } while (x > 1);
+    }
+  }
+  const res: Set<string> = new Set([])
+
   const matrix: Tile[][] = []
   for (const line of data) {
     const splittedLines = line.split('') as Tile[]
@@ -48,18 +74,55 @@ export function solver(data: string[]) {
       const pos = row[x];
       if (isGuard(pos)) {
         if (pos === "<" || pos === ">") {
-          // const currentRow = selectMatrixRow(matrix, y)
+          const currentRow = selectMatrixRow(matrix, y)
+          const obstacle = currentRow.indexOf("#")
+
+          const startIdx = pos === "<" ? 0 : x
+          const endIdx = pos === '<' ? x : undefined
+          const frontLine = currentRow.slice(startIdx, endIdx)
+          const front = frontLine.indexOf("#")
+
+          if (front === -1) { // end
+            addTiles({ x, y }, { x: x + frontLine.length, y })
+            return res.size + 1
+          }
+
+          const obstacleCoordinates = { x: obstacle, y }
+          addTiles({ y, x }, obstacleCoordinates)
+
+          const offSet = obstacle > x ? -1 : 1
+          const nextPos = nextGuard(pos)
+          matrix[y][x] = "."
+          matrix[y][obstacle + offSet] = nextPos
+          x = 0
         }
         if (pos === "^" || pos === "v") {
           const currentCol = selectMatrixColumn(matrix, x)
           const obstacle = currentCol.indexOf("#")
-          console.log(pos, { x, y }, obstacle)
+
+          const startIdx = pos === "^" ? 0 : y
+          const endIdx = pos === '^' ? y : undefined
+          const frontLine = currentCol.slice(startIdx, endIdx)
+          const front = frontLine.indexOf("#")
+
+          if (front === -1) { // end
+            addTiles({ x, y }, { x, y: y + frontLine.length })
+            return res.size + 1
+          }
+
+          const obstacleCoordinates = { y: obstacle, x }
+          addTiles({ y, x }, obstacleCoordinates)
+
+          const offSet = obstacle > y ? -1 : 1
+          const nextPos = nextGuard(pos)
+          matrix[y][x] = "."
+          matrix[obstacle + offSet][x] = nextPos
+          y = 0
+
         }
       }
     }
-
   }
-  return 1;
 }
 
 // main();
